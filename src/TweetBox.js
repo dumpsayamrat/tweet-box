108a2d0e4a5c164300d08e1fab0eba88f46297a1
dumpsayamrat <sayamrat.kt@gmail.com>
2017-08-18 23:36:00 +0700

    Tweet-2

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -21,26 +21,12 @@ const TweetBox = props => (
   <div style={{ height: 150, display: 'flex' }}>
     <div style={{ flex: 1, position: 'relative' }}>
       <img
-        style={{ 
-          borderRadius: '50%',
-          left: 28,
-          position: 'absolute',
-          top: 13,
-          width: 64,
-          height: 64 
-        }}
+        style={styles.imageStyle}
         src={props.image} 
         alt={props.name}
       />
       <h6
-        style={{ 
-          color: '#585858',
-          left: 35,
-          position: 'absolute',
-          top: 60,
-          width: 64,
-          height: 64 
-        }}
+        style={styles.usernameStyle}
       >
         @{props.username}
       </h6>
@@ -49,39 +35,22 @@ const TweetBox = props => (
       <textarea
         placeholder={'What\'s happening?'}
         wrap="hard"
-        style={{
-          resize: 'none',
-          width: '95%',
-          display: 'inline-block',
-          borderColor: '#55acee',
-          minHeight: 60,
-          fontSize: 16,
-          padding: 10,
-        }}
+        style={styles.textareaStyle}
         onChange={props.onMessageChange}
         value={props.message}
       />
       <div style={{ alignSelf: 'flex-end', paddingTop: 10, display: 'flex', flexDirection: 'row' }}>
         <h4
           style={{
-            alignSelf: 'center',
-            marginRight: 10,
-            color: props.textRemaining < 0 ? 'red' : '#585858',
+            ...styles.textRemainingStyle, 
+            color: props.textRemaining < 0 ? 'red' : '#585858'
           }}
         >
           {props.textRemaining}
         </h4>
         <button
           style={{
-            background: '#A7DBFB',
-            border: 'none',
-            width: 80,
-            padding: '6px 16px',
-            borderRadius: 100,
-            color: 'white',
-            fontWeight: 700,
-            fontSize: 14,
-            marginRight: 10,
+            ...styles.buttonStyle,
             cursor: props.isButtonDisable ? 'default' : 'pointer',
             opacity: props.isButtonDisable ? .3 : 1,
           }}
@@ -93,5 +62,48 @@ const TweetBox = props => (
     </div>
   </div>
 );
+
+const styles = {
+  usernameStyle: { 
+    color: '#585858',
+    left: 35,
+    position: 'absolute',
+    top: 60,
+    width: 64,
+    height: 64 
+  },
+  textareaStyle: {
+    resize: 'none',
+    width: '95%',
+    display: 'inline-block',
+    borderColor: '#55acee',
+    minHeight: 60,
+    fontSize: 16,
+    padding: 10,
+  },
+  imageStyle: { 
+    borderRadius: '50%',
+    left: 28,
+    position: 'absolute',
+    top: 13,
+    width: 64,
+    height: 64 
+  },
+  textRemainingStyle: {
+    alignSelf: 'center',
+    marginRight: 10,
+  },
+  buttonStyle: {
+    background: '#A7DBFB',
+    border: 'none',
+    width: 80,
+    padding: '6px 16px',
+    borderRadius: 100,
+    color: 'white',
+    fontWeight: 700,
+    fontSize: 14,
+    marginRight: 10,
+  },
+};
 
 export default enhance(TweetBox);
