8aaf9781deecd588765cf57425558fcdffa4b96f
dumpsayamrat <sayamrat.kt@gmail.com>
2017-08-18 23:18:55 +0700

    Tweet-2

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -1,9 +1,97 @@
 import React from 'react';
+import { compose, mapProps, flattenProp } from 'recompose';
 
-const TweetBox = () => (
-  <div>
-    Tweet Box
+const enhance = compose(
+  mapProps(({ limit, message, ...rest }) => ({
+    ...rest,
+    limit,
+    message,
+    textRemaining: limit - message.length
+  })),
+  mapProps(({ textRemaining, message, ...rest }) => ({
+    ...rest,
+    textRemaining,
+    message,
+    isButtonDisable: !message.length || textRemaining < 0,
+  })),
+  flattenProp('user'),
+);
+
+const TweetBox = props => (
+  <div style={{ height: 150, display: 'flex' }}>
+    <div style={{ flex: 1, position: 'relative' }}>
+      <img
+        style={{ 
+          borderRadius: '50%',
+          left: 28,
+          position: 'absolute',
+          top: 13,
+          width: 64,
+          height: 64 
+        }}
+        src={props.image} 
+        alt={props.name}
+      />
+      <h6
+        style={{ 
+          color: '#585858',
+          left: 35,
+          position: 'absolute',
+          top: 60,
+          width: 64,
+          height: 64 
+        }}
+      >
+        @{props.username}
+      </h6>
+    </div>
+    <div style={{ flex: 5, padding: 10, display: 'flex', flexDirection: 'column' }}>
+      <textarea
+        placeholder={'What\'s happening?'}
+        wrap="hard"
+        style={{
+          resize: 'none',
+          width: '95%',
+          display: 'inline-block',
+          borderColor: '#55acee',
+          minHeight: 60,
+          fontSize: 16,
+          padding: 10,
+        }}
+        onChange={props.onMessageChange}
+        value={props.message}
+      />
+      <div style={{ alignSelf: 'flex-end', paddingTop: 10, display: 'flex', flexDirection: 'row' }}>
+        <h4
+          style={{
+            alignSelf: 'center',
+            marginRight: 10,
+            color: props.textRemaining < 0 ? 'red' : '#585858',
+          }}
+        >
+          {props.textRemaining}
+        </h4>
+        <button
+          style={{
+            background: '#A7DBFB',
+            border: 'none',
+            width: 80,
+            padding: '6px 16px',
+            borderRadius: 100,
+            color: 'white',
+            fontWeight: 700,
+            fontSize: 14,
+            marginRight: 10,
+            cursor: props.isButtonDisable ? 'default' : 'pointer',
+            opacity: props.isButtonDisable ? .3 : 1,
+          }}
+          disabled={props.isButtonDisable}
+        >
+          Tweet
+        </button>
+      </div>
+    </div>
   </div>
 );
 
-export default TweetBox;
+export default enhance(TweetBox);
--- src/TweetHomeRecompose.js
+++ src/TweetHomeRecompose.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { compose, withState, withHandlers } from 'recompose';
 import Tweets from './Tweets';
 import TweetBox from './TweetBox';
 
-const TweetHome = () => (
+const limit = 140;
+const user = {
+  name: 'John Doe',
+  username: 'Ah_john',
+  image: './dog.jpg'
+};
+
+const enhance = compose(
+  withState('limit', 'updateLimit', limit),
+  withState('user', 'updateUser', user),
+  withState('message', 'updateMessage', ''),
+  withHandlers({
+    onMessageChange: ({ updateMessage }) => ({ target }) => updateMessage(() => target.value)
+  }),
+);
+
+const TweetHome = props => (
   <div style={{ display: 'flex', textAlign: 'left', width: 680, margin: 'auto', flexDirection: 'column' }}>
     <div style={{ flex: 1, background: '#F4FAFE' }}>
-      <TweetBox />
+      <TweetBox
+        user={props.user}
+        message={props.message}
+        onMessageChange={props.onMessageChange}
+        limit={props.limit}
+      />
     </div>
     <div style={{ flex: 1, background: 'white', display: 'flex', flexDirection: 'column-reverse' }}>
       <Tweets />
@@ -13,4 +35,4 @@ const TweetHome = () => (
   </div>
 );
 
-export default TweetHome;
+export default enhance(TweetHome);
